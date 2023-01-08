6e0efb9edb89cb83690a04e6a46f5ab01dfc11e6
arabian giga chad <110537092+IIIIIIIIIIITRBIIIIIIIII@users.noreply.github.com>
2023-01-08 16:02:07 +0100

    first commit

---
+++ src/UserList.jsx
@@ -0,0 +1,12 @@
+import React from 'react'
+import { useState } from 'react'
+const [data] = useState({data});
+const UserList = () => {
+  return (
+    <div>
+
+    </div>
+  )
+}
+
+export default UserList
